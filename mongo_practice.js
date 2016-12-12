1801b46f3d924e9929273f3c786a9596ca6551c5
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-12 11:08:33 -0500

    Added files for mongodb practice

---
+++ mongo_practice.js
@@ -0,0 +1,20 @@
+
+var python = {
+  language: 'Python',
+  inventors: 'Guido van Rossum',
+  firstAppeared: '1991',
+  paradigms: [
+    'object-oriented',
+    'imperative',
+    'functional',
+    'procedural',
+    'reflective'
+  ],
+  typingDiscipline: [
+    'duck',
+    'dynamic',
+    'strong',
+    'gradual'
+  ]
+};
+db.my_database.insert(python);
