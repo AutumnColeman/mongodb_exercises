9869ce20cf7c83d97491e416dd746ddc901e2435
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-12 11:51:37 -0500

    Up to 5

--- mongo_practice.js
+++ mongo_practice.js
@@ -60,3 +60,47 @@ var java = {
   ]
 };
 db.my_database.insert(java);
+
+//Haskell
+var haskell = {
+  language: 'Haskell',
+  inventors: 'Simon Peyton Jones',
+};
+db.my_database.insert(haskell);
+
+//Closure
+var closure = {
+  language: 'Closure',
+  inventors: 'Rich Hickey',
+  paradigms: [
+     'functional'
+  ],
+  typingDiscipline: [
+    'dynamic',
+    'strong'
+  ]
+};
+db.my_database.insert(closure);
+
+//Euphoria
+var euphoria = {
+  language: 'Euphoria',
+  inventors: 'Robert Craig',
+  typingDiscipline: [
+    'static',
+    'dynamic'
+  ]
+};
+db.my_database.insert(euphoria);
+
+//1. Find a programming language by its object ID
+db.my_database.find({_id: ObjectId("584ed11e4f3548cf68cbb390")}).pretty()
+
+//2. Find all object-oriented programming languages.
+db.my_database.find({paradigms: /object-oriented.*/i}).pretty()
+
+//3. Find all functional programming languages.
+db.my_database.find({paradigms: 'functional'}).pretty()
+
+//4. Find all programming languages that have "duck-typing".
+db.my_database.find({typingDiscipline: 'duck'}).pretty()
